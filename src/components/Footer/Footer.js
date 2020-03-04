002bcf4834dea5defa09449bf87799e5ab32b20c
nataliegarate <natalieegarate@hotmail.com>
2020-03-03 18:08:50 -0800

    added needed content to footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+export default function footer() {
+  return (
+    <div className="footer">
+      <div className="contact-footer">
+        <h2> CONTACT US: VALLEYINSTALLERS@YAHOO.COM | 406.892.4871</h2>
+      </div>
+      <div className="author-footer">
+        <div id="space"></div>
+        <div id="text">Website Built by Natalie Garate and Lillian Gales</div>
+      </div>
+    </div>
+  );
+}
