fcde22d85ba98496d81f8351e570f3a00fbcca22
Lillian Gales <lilliangales@ymail.com>
2020-03-04 17:05:35 -0800

    added mail to on contact us

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,23 +10,20 @@ function Header(props) {
           <p id="name-header">Valley Installers</p>
           <div id="right-header">
             <p id="about-header">About</p>
-            <div id="contact-container">
+            <a id="contact-container" href="mailto:valleyinstallers@yahoo.com">
+              {/* <div id="contact-container"> */}
               <i class="far fa-envelope"></i>
               <p id="contact-header">Contact Us</p>
-            </div>
+              {/* </div> */}
+            </a>
           </div>
         </div>
       </div>
 
       <div className="image-container">
-        {/* <img
-          alt="placeholder"
-          src="https://wearemortar.com/wp-content/uploads/2019/09/placeholder.png"
-        /> */}
         <div id="image-text">
           <h1>Quality Laminte Countertops Serving The Greater Flathead Area</h1>
           <Link to="/about-us">Learn More About Us →</Link>
-          {/* <h2>Learn More About Us →</h2> */}
         </div>
       </div>
     </header>
